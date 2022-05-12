e625d2cbbc13e3daa18a052df1110183eaddaaf2
m93a <m93a.cz@gmail.com>
2022-05-12 15:15:46 +0200

    seems to work (epic w)

--- src/lib/Plot.svelte
+++ src/lib/Plot.svelte
@@ -1,16 +1,41 @@
-<script lang="ts">
-    import { onMount } from 'svelte';
-    let Plotly: null | typeof import("plotly.js");
-
-    export let element: HTMLDivElement = null as never;
-
-    console.log(element);
-
-    onMount(async () => {
-        (window as any).global = window;
-        Plotly = await import("plotly.js");
-        console.log('loaded', Plotly);
-    });
+<script context="module" lang="ts">
+  import type { Data, Layout, Config, PlotlyHTMLElement } from 'plotly.js';
+  export type { Data, Layout, Config, PlotlyHTMLElement };
 </script>
 
-<div bind:this={element}></div>
+<script lang="ts">
+  import { onMount, onDestroy } from 'svelte';
+
+  let Plotly: null | typeof import('plotly.js-dist');
+  let datarevision = 0;
+
+  // set up
+  onMount(async () => {
+    (window as any).global = window;
+    Plotly = await import('plotly.js-dist');
+  });
+
+  // bind props
+  export let element: HTMLDivElement = null as never;
+  export let plot: PlotlyHTMLElement = null as never;
+
+  // input props
+  export let data: Data[];
+  export let layout: Partial<Layout> | undefined = undefined;
+  export let config: Partial<Config> | undefined = undefined;
+  let className = '';
+  export { className as class };
+
+  // update
+  $: data, (datarevision = (datarevision + 1) % 1000);
+  $: layoutWithRevison = { datarevision, ...layout };
+  $: configWithoutLogo = { displaylogo: false, ...config };
+  $: Plotly?.react(element, data, layoutWithRevison, configWithoutLogo).then(p => (plot = p));
+
+  // destroy
+  onDestroy(() => {
+    Plotly?.purge(plot);
+  });
+</script>
+
+<div class={className} bind:this={element} />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,41 @@
 <script lang="ts">
-    import Plot from "$lib/Plot.svelte";
+  import Plot from '$lib/Plot.svelte';
+  import type { Data } from '$lib/Plot.svelte';
+
+  let y0 = 1;
+
+  let data: Data[];
+  $: data = [
+    {
+      x: [1, 2, 3, 4, 5],
+      y: [y0, 2, 4, 8, 16]
+    }
+  ];
+
+  function addData() {
+    data.push({
+      x: [1, 2, 3, 4, 5],
+      y: Array(5)
+        .fill(0)
+        .map((_) => 10 * Math.random())
+    });
+    data = data;
+  }
+
+  function changeY0() {
+    y0++;
+  }
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<Plot />
+<Plot
+  {data}
+  layout={{
+    margin: { t: 0 }
+  }}
+/>
+
+<button on:click={addData}>Add trace</button>
+<button on:click={changeY0}>Increase y<sub>0</sub></button>
