d707d06f25a54cbab57399fe38fefbaf0044d64f
m93a <m93a.cz@gmail.com>
2022-05-12 18:24:20 +0200

    fill parent, debouncing

--- src/lib/Plot.svelte
+++ src/lib/Plot.svelte
@@ -1,41 +1,136 @@
 <script context="module" lang="ts">
   import type { Data, Layout, Config, PlotlyHTMLElement } from 'plotly.js';
   export type { Data, Layout, Config, PlotlyHTMLElement };
+  export type FillParent = boolean | 'width' | 'height';
+  import type { DebounceSettings } from 'lodash';
+  export interface DebounceOptions extends DebounceSettings {
+    wait: number;
+  }
 </script>
 
 <script lang="ts">
   import { onMount, onDestroy } from 'svelte';
+  import { debounce as debouncify } from 'lodash';
+  import { browser } from '$app/env';
+  type not = undefined | null;
 
-  let Plotly: null | typeof import('plotly.js-dist');
-  let datarevision = 0;
+  const nextFrame = browser ? requestAnimationFrame : () => void 0;
+
+  async function loadPlotly() {
+    if (!browser) return;
+    if (libPlotly === undefined) {
+      const p = await import('plotly.js-dist');
+      if (libPlotly === undefined) libPlotly = p;
+    }
+  }
+
+  const DEFAULT_WIDTH = 500;
+  const DEFAULT_HEIGHT = 300;
+
+  // bind props
+  export let element: HTMLDivElement | not = undefined;
+  export let plot: PlotlyHTMLElement | not = undefined;
+
+  // input props
+  export let libPlotly: typeof import('plotly.js-dist') | null | undefined = undefined;
+  export let data: Data[];
+  export let layout: Partial<Layout> | undefined = undefined;
+  export let config: Partial<Config> | undefined = undefined;
+  export let fillParent: FillParent = false;
+  export let debounce: number | DebounceOptions = 0;
+  let className = '';
+  export { className as class };
 
   // set up
   onMount(async () => {
     (window as any).global = window;
-    Plotly = await import('plotly.js-dist');
+    loadPlotly();
   });
 
-  // bind props
-  export let element: HTMLDivElement = null as never;
-  export let plot: PlotlyHTMLElement = null as never;
+  // state props
+  let datarevision = 0;
+  let previousLib = libPlotly;
+  let width: number = DEFAULT_WIDTH;
+  let height: number = DEFAULT_HEIGHT;
 
-  // input props
-  export let data: Data[];
-  export let layout: Partial<Layout> | undefined = undefined;
-  export let config: Partial<Config> | undefined = undefined;
-  let className = '';
-  export { className as class };
-
-  // update
+  // updates
+  $: debounceWait = typeof debounce === 'object' ? debounce.wait : debounce ?? 0;
+  $: debounceOptions = typeof debounce === 'object' ? debounce : {};
   $: data, (datarevision = (datarevision + 1) % 1000);
-  $: layoutWithRevison = { datarevision, ...layout };
-  $: configWithoutLogo = { displaylogo: false, ...config };
-  $: Plotly?.react(element, data, layoutWithRevison, configWithoutLogo).then(p => (plot = p));
+  $: layout_ = { datarevision, width, height, ...layout };
+  $: config_ = { displaylogo: false, ...config };
+  $: draw(libPlotly, element, data, layout_, config_);
+  $: {
+    if (element && previousLib !== libPlotly) previousLib?.purge(element);
+    previousLib = libPlotly;
+    loadPlotly();
+  }
+
+  const drawUndebounced = (
+    lib: typeof libPlotly,
+    e: HTMLDivElement | not,
+    d: Data[],
+    l: Partial<Layout>,
+    c: Partial<Config>
+  ) => {
+    if (e) lib?.react(e, d, l, c).then(p => (plot = p));
+  };
+
+  $: draw = debouncify(drawUndebounced, debounceWait, debounceOptions);
 
   // destroy
-  onDestroy(() => {
-    Plotly?.purge(plot);
-  });
+  onDestroy(() => element && libPlotly?.purge(element));
+
+  // autosizing
+  $: fillParent, nextFrame(onResize);
+  $: fillParentWidth = fillParent === true || fillParent === 'width';
+  $: fillParentHeight = fillParent === true || fillParent === 'height';
+  $: parent = element?.parentElement;
+  let lastParent: typeof parent = null;
+  $: {
+    parentMounted(parent);
+    parentUnmounted(lastParent);
+    lastParent = parent;
+  }
+
+  let resizeObserver: ResizeObserver | not;
+  onMount(() => (resizeObserver = new ResizeObserver(onResize)));
+  const parentMounted = (parent: Element | not) => parent && resizeObserver?.observe(parent);
+  const parentUnmounted = (parent: Element | not) => parent && resizeObserver?.unobserve(parent);
+
+  function onResize() {
+    if (!parent || !element) return;
+
+    const { offsetHeight, offsetWidth } = parent;
+    const { paddingLeft, paddingTop, paddingRight, paddingBottom } =
+      window.getComputedStyle(parent);
+
+    const maxWidth = offsetWidth - parseInt(paddingLeft) - parseInt(paddingRight);
+    const maxHeight = offsetHeight - parseInt(paddingTop) - parseInt(paddingRight);
+
+    width = fillParentWidth ? maxWidth : DEFAULT_WIDTH;
+    height = fillParentHeight ? maxHeight : DEFAULT_HEIGHT;
+  }
 </script>
 
-<div class={className} bind:this={element} />
+<div
+  class={className}
+  class:fillParent
+  class:fillParentWidth
+  class:fillParentHeight
+  bind:this={element}
+/>
+
+<style lang="scss">
+  .fillParent {
+    overflow: visible;
+  }
+  .fillParentWidth {
+    width: 0 !important;
+    max-width: 0 !important;
+  }
+  .fillParentHeight {
+    height: 0 !important;
+    max-height: 0 !important;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   import Plot from '$lib/Plot.svelte';
-  import type { Data } from '$lib/Plot.svelte';
+  import type { Data, FillParent, DebounceOptions } from '$lib/Plot.svelte';
 
   let y0 = 1;
+  let useDefaultLib = true;
+  let fillParent: FillParent = false;
+  let debounce: DebounceOptions | number | undefined;
 
   let data: Data[];
   $: data = [
@@ -17,7 +20,7 @@
       x: [1, 2, 3, 4, 5],
       y: Array(5)
         .fill(0)
-        .map((_) => 10 * Math.random())
+        .map(_ => 10 * Math.random())
     });
     data = data;
   }
@@ -27,15 +30,39 @@
   }
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
 <Plot
   {data}
+  {fillParent}
+  {debounce}
   layout={{
     margin: { t: 0 }
   }}
+  libPlotly={useDefaultLib ? undefined : null}
 />
 
-<button on:click={addData}>Add trace</button>
-<button on:click={changeY0}>Increase y<sub>0</sub></button>
+<div class="controls">
+  <button on:click={addData}>Add trace</button>
+  <button on:click={changeY0}>Increase y<sub>0</sub></button>
+  <button on:click={() => (useDefaultLib = !useDefaultLib)}>Swap lib</button>
+  <button on:click={() => (fillParent = false)}>Fixed size</button>
+  <button on:click={() => (fillParent = true)}>Fill parent</button>
+  <button on:click={() => (fillParent = 'width')}>Fill width</button>
+  <button on:click={() => (fillParent = 'height')}>Fill height</button>
+  <button on:click={() => (debounce = undefined)}>Don't debounce</button>
+  <button on:click={() => (debounce = 500)}>Debounce 500ms</button>
+  <button on:click={() => (debounce = { wait: 500, maxWait: 2000 })}>Debounce 500ms, max wait 2s</button>
+</div>
+
+<style lang="scss">
+  :global(html, body, body > div) {
+    padding: 0;
+    margin: 0;
+    width: 100vw;
+    height: 100vh;
+  }
+  .controls {
+    position: absolute;
+    left: 0;
+    bottom: 0;
+  }
+</style>
