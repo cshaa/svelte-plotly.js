1cf3516c33e481a1b163333f74aba8487b9c5952
m93a <m93a.cz@gmail.com>
2022-05-12 09:53:00 +0200

    oof

---
+++ lib/Plot.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    let Plotly: null | typeof import("plotly.js");
+
+    export let element: HTMLDivElement = null as never;
+
+    console.log(element);
+
+    onMount(async () => {
+        Plotly = await import("plotly.js");
+        console.log('loaded', Plotly);
+    });
+</script>
+
+<div bind:this={element}></div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+    import Plot from "../../lib/Plot.svelte";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Plot />
