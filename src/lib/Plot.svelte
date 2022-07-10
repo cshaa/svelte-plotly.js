7804ce9c11bd19be4800b6f3605aa7cef0c191b3
Mick Chanthaseth <mchanthaseth@gmail.com>
2022-07-10 11:05:04 -0700

    added plotly events

--- src/lib/Plot.svelte
+++ src/lib/Plot.svelte
@@ -15,7 +15,7 @@
   type not = undefined | null;
 
   const dispatch = createEventDispatcher();
-  
+
   const nextFrame = browser ? requestAnimationFrame : () => void 0;
 
   async function loadPlotly() {
@@ -75,17 +75,47 @@
     l: Partial<Layout>,
     c: Partial<Config>
   ) => {
-    if (e) {
-      lib?.react(e, d, l, c)
-          .then((p) => (plot = p))
-          .then(() => {
-            e.on("plotly_relayout", (e) => {
-              dispatch("relayout", {
-                data: e,
-              });
-            });
+    lib
+      ?.react(e, d, l, c)
+      .then(p => (plot = p))
+      .then(() => {
+        [
+          'plotly_click',
+          'plotly_hover',
+          'plotly_unhover',
+          'plotly_selecting',
+          'plotly_selected',
+          'plotly_restyle',
+          'plotly_relayout',
+          'plotly_relayouting',
+          'plotly_clickannotation',
+          'plotly_animatingframe',
+          'plotly_legendclick',
+          'plotly_legenddoubleclick',
+          'plotly_sliderchange',
+          'plotly_sliderend',
+          'plotly_sliderstart',
+          'plotly_sunburstclick',
+          'plotly_event',
+          'plotly_beforeplot',
+          'plotly_afterexport',
+          'plotly_afterplot',
+          'plotly_animated',
+          'plotly_animationinterrupted',
+          'plotly_autosize',
+          'plotly_beforeexport',
+          'plotly_deselect',
+          'plotly_doubleclick',
+          'plotly_framework',
+          'plotly_redraw',
+          'plotly_transitioning',
+          'plotly_transitioninterrupted'
+        ].forEach(element => {
+          e.on(element, event => {
+            dispatch(element, event);
           });
-     }
+        });
+      });
   };
 
   $: draw = debouncify(drawUndebounced, debounceWait, debounceOptions);
