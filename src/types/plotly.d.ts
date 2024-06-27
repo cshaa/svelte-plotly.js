ac40d166285a99dafb0c0347da2274816b3469ea
Richard Ivánek <richard@ivanek.dev>
2024-06-27 10:38:34 +0200

    feat: update to sveltekit v2, move to npm, update all dependencies, correctly specify exports, resolves #9

--- src/types/plotly.d.ts
+++ src/types/plotly.d.ts
@@ -1,3 +1,3 @@
-declare module "plotly.js-dist" {
-    export * from "plotly.js";
+declare module 'plotly.js-dist' {
+  export * from 'plotly.js';
 }
--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
-	kit: {
-		adapter: adapter()
-	}
-};
-
-export default config;
+export default {
+  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+}
